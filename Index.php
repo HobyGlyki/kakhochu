227b2e6ea0f11ab4889939ef3d9b11559d96cf68
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-27 21:11:49 +0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -3,14 +3,14 @@
     // Подключение файла соединения с БД
 include_once 'db.class.php';
     // хост БД
-define('db_host','localhost');
+define('db_host','localhost 3360');
     // Имя БД
 define('db_name','BDate');
     // Пользователь БД
 define('db_user','root');
 // Пароль БД
 define('db_pass','admin_password');
-
+$db = new DB_class(db_host,db_name,db_user,db_pass);
 
 echo "Ваше имя: ";
 ?>
