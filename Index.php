05efe948327580635efadc79ac5e180ef6cb8360
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-27 21:43:32 +0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -13,13 +13,13 @@ define('db_pass','admin_password');
 
 define('pi', 3);
 
-
+ 
 $suka = 3* pi;
 
 $db = new DB_class(db_host,db_name,db_user,db_pass);
 
 
-
+print_r($query[0]['task_id']);
 
 
 echo "Ваше имя: " .$suka;
