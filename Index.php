3e065a7af9f87bf0eebf887a6b827ddbf198b35c
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-27 22:01:55 +0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -13,15 +13,9 @@ define('db_pass','admin_password');
 
 define('pi', 3);
 
- 
-$plosh = 3*3* pi;
-
 $db = new DB_class(db_host,db_name,db_user,db_pass);
-
+$query = $db->select(true,"*","bdname","example='".$example."'");
 
 print_r($query[0]['task_id']);
-
-
-echo "площадь " .$plosh;
 
 ?>
