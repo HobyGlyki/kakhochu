07e9a6831e16a9d60b0a9ab5fda8bcc38f9ed57d
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-27 21:00:08 +0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,23 +1,6 @@
 <?php 
     // Подключение файла соединения с БД
-include_once 'db.class.php';
-    // хост БД
-define('db_host','localhost');
-    // Имя БД
-define('db_name','BDate');
-    // Пользователь БД
-define('db_user','root');
-// Пароль БД
-define('db_pass','admin_password');
-
-    // Обявление класса для подключения к бд
-$db = new DB_class(db_host,db_name,db_user,db_pass);
-
-
-    // Создаем соединение
-$query = $db->select(true,"*","bdname","example='".$example."'");
-print_r($query[0]['название колонки']);
-print_r($query[1]['название колонки']);
+echo "Да я ебал это всё
 
 
 
