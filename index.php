e03c9f6a59ec6d7a1756553c97066baae66b081c
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 00:19:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -150,7 +150,7 @@ $where1= "`task_id`='$example'"; #условие
 #$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
 #echo ( $update_sql1);
 $query = $db->update($name, $set1, $where1);
-if($chede <> $chedn){header("Refresh:0");}
+if($chede <> $chedn) {header("Refresh:0");}}
 
 
 ?>
