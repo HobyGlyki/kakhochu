0acacb43cab27946ac6f3b75822041df8d7754bc
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 01:42:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,15 +15,16 @@ define('db_pass','admin_password');
 
 $db = new DB_class(db_host,db_name,db_user,db_pass);
 $query = $db->select(true,"*","tasks","");
-
+$chedn=[]
+$chedn=[];
 for($hi=0;$hi<count($query);$hi++) {
 if(not($query[$hi]['task_complete'])){
-	$chedn[$hi] ="не сделанно ";
-	$ched[$hi] = " ";
+	$chedn[] = "не сделанно ";
+	$ched[] = " ";
 }
 else{
-	 $ched[$hi] = "checked";
-         $chedn[$hi] ="сделанно ";
+	 $ched[] = "checked";
+         $chedn[] ="сделанно ";
        }
 }
  
