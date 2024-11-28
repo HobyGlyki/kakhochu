e842150ebebb2961d24e361e64dbbb8b187cc614
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-28 19:31:29 +0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -69,9 +69,9 @@ $name="`tasks`";
 $set1="`$text` = '$step'";
 $where1= "'task_id'='$example'";
 $where = "WHERE ".$where1;
-echo ($name, $set1, $where1);
+echo ($name. $set1. $where1);
 
-$query = $db->update($name. $set1. $where1);
+$query = $db->update($name, $set1, $where1);
 ?>
 
 </body>
