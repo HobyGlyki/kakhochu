c70710fd0860c283a827be65940dc07c5981e611
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 02:36:23 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -22,7 +22,7 @@ $text = "task_complete"; # Что поменять
 $step = 1;
 $chedn ="привет";# На что поменять
 $set1="`$text` = '$step'"; #соединить Text и Step
-$example =1; #Значение условия
+$example =0; #Значение условия
 $where1= "`task_id`='$example'"; #условие
 
 $where = "WHERE ".$where1;
