3b10cd16f5f02b8efd9d02e76af21fa9b1ec1558
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 00:27:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -146,11 +146,10 @@ $where1= "`task_id`='$example'"; #условие
 
 $where = "WHERE ".$where1;
 $update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
-echo ( $update_sql1);
+
 $query = $db->update($name, $set1, $where1);
+echo ( $update_sql1);
 }
-
-
 ?>
    </div>
 
