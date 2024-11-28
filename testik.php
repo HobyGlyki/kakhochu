2018f7dd3658d3ec3deac40d4f376027bd630a62
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 02:34:07 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -2,12 +2,25 @@
 
 // Подключение файла соединения с БД
 
+ // Подключение файла соединения с БД
+include_once 'db.class.php';
 
+
+ // хост БД
+define('db_host','localhost:3306');
+ // Имя БД
+define('db_name','dbate');
+// Пользователь БД
+define('db_user','admin');
+// Пароль БД
+define('db_pass','admin_password');
+
+$db = new DB_class(db_host,db_name,db_user,db_pass);
 
 $name="`tasks`"; # Название таблицы
 $text = "task_complete"; # Что поменять
 $step = 1;
-$chedn ="привет";}# На что поменять
+$chedn ="привет";# На что поменять
 $set1="`$text` = '$step'"; #соединить Text и Step
 $example =1; #Значение условия
 $where1= "`task_id`='$example'"; #условие
