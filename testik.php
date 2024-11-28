39b56fc893f834daa08d142f86c197f4b95f0050
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 02:27:57 +0300

    Add files via upload

---
+++ testik.php
@@ -0,0 +1,27 @@
+<?php
+
+// Подключение файла соединения с БД
+include_once 'db.class.php';
+
+
+// хост БД
+define('db_host','localhost:3306');
+// Имя БД
+define('db_name','dbate');
+// Пользователь БД
+define('db_user','admin');
+// Пароль БД
+define('db_pass','admin_password');
+
+$db = new DB_class(db_host,db_name,db_user,db_pass);
+
+$step = 1;
+$chedn ="привет";}# На что поменять
+$set1="`$text` = '$step'"; #соединить Text и Step
+$example =1; #Значение условия
+$where1= "`task_id`='$example'"; #условие
+
+$where = "WHERE ".$where1;
+
+$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
+?>
