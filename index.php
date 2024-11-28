de1f74cfb3ce26540b1302939ae70ec0c5a4ded1
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 02:51:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ else{
  	</div>
 </div>
 <div class="main-text">
-	<form action=" " method="post">
+	<form action="" method="post">
 		<?php for($i=0;$i<count($query);$i++) { ?>
 	        <div class="task">
             		<div class="task-control", a>
