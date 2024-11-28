5b6aff547dc9b9ba84fc170f4e6172e78948bb58
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-28 18:03:34 +0300

    Update Index.php

--- Index.php
+++ Index.php
@@ -36,7 +36,7 @@ $query = $db->select(true,"*","tasks","");
         ?>
         <div class="task">
             <div class="task-control", a>
-                <input type="checkbox" name="check" checked=<?php echo $query[$i]['task_complete'] ?>>	    	
+                <input type="checkbox" name="check">	    	
             </div>
             <div class="task-text"><?php echo $query[$i]['task_text'] ?></div>
             <div class="task-date">
