d4001ae82b2b95bbf478a6322ae8f276f4ad1c64
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 00:31:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -131,7 +131,7 @@ echo $chedn. $query[$i]['task_complete'];
 
 <?php
 echo $_POST["save"];
-for($j=0;$i<count($query);$i++) {
+for($j=0;$i<count($query);$j++) {
 $name="`tasks`"; # Название таблицы
 $text = "task_complete"; # Что поменять
 if($_POST["check$j"] == null){
