151b5803104755ca24b7ca4bc9277a7a805a6ee7
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 02:33:04 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -1,18 +1,7 @@
 <?php
 
 // Подключение файла соединения с БД
-include_once 'db.class.php';
 
-
-// хост БД
-define('db_host','localhost:3306');
-// Имя БД
-define('db_name','dbate');
-// Пользователь БД
-define('db_user','admin');
-// Пароль БД
-define('db_pass','admin_password');
-]
 
 
 $name="`tasks`"; # Название таблицы
