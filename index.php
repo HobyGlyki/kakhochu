432d69c22a62298ae8d0de3d4fe51db36a710398
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-28 22:07:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -139,8 +139,8 @@ $set1="`$text` = '$step'"; #соединить Text и Step
 $example = "1"; #Значение условия
 $where1= "`task_id`='$example'"; #условие
 
-#$where = "WHERE ".$where1;
-$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where1."";
+$where = "WHERE ".$where1;
+$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
 echo ( $update_sql1);
 $query = $db->update($name, $set1, $where1);
 ?>
