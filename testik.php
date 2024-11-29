2e62145497c600ea25a99c2c21fc3627acced08f
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 03:25:48 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -28,15 +28,7 @@ if($_POST["check0"] == null){
 else{
 	$step = 1;
         $chedn ="привет";}# На что поменять
-$set1="`$text` = '$step'"; #соединить Text и Step
-$example =1; #Значение условия
-$where1= "`task_id`='$example'"; #условие
-
-#$where = "WHERE ".$where1;
-
-#$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
-$query = $db->update($name, $set1, $where1);}
-header("Refresh:0");}
+}}
 
 
 ?>
