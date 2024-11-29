df6dc92f5d7c7f86ab1ba76d61e3b16851682aab
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-29 03:20:51 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -16,25 +16,15 @@ define('db_user','admin');
 define('db_pass','admin_password');
 
 $db = new DB_class(db_host,db_name,db_user,db_pass);
+$query = $db->select(true,"*","tasks","");
+
 if ($_POST){
 $name="`tasks`"; # Название таблицы
 $text = "task_complete"; # Что поменять
 
 for($j=0;$j<count($query);$j++) {
 if($_POST["check0"] == null){
-	$step = 0; 
-        $chedn ="не сделанно";}
-else{
-	$step = 1;
-        $chedn ="привет";}# На что поменять
-$set1="`$text` = '$step'"; #соединить Text и Step
-$example =1; #Значение условия
-$where1= "`task_id`='$example'"; #условие
-
-#$where = "WHERE ".$where1;
-
-#$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
-$query = $db->update($name, $set1, $where1);}
+}
 header("Refresh:0");}
 
 ?>
