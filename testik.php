d85148c4549786ae191e00702e0379cf9f6f32ff
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-30 13:00:17 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -34,35 +34,12 @@ $datetask = $_POST["timenew"];
 $step = "'$idtask', '$texttask', '$datetask', '0'";
 $queryup = $db->insert($name,$completet ,$step);
 }
-if(1==0){	
-for($j=0;$j<count($query);$j++) {
-	$completet = "task_complete"; # Что поменять
-	$example =$j+1; #Значение условия
-	$where1= "`task_id`='$example'"; #условие
-		$step = $_POST["check$j"]-1;# На что поменять
-		$set1="`$completet` = '$step'"; #соединить Text и Step
-#$where = "WHERE ".$where1;
-#$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
-		#$queryup = $db->update($name, $set1, $where1);
 
-	if(($_POST["del"]==$j+1) and ($maxx > 1)){
-		$queryup = $db->delete($name, $where1);
-		$queryup = $db->select(true,"*","tasks","");
-		for($d=$j;$d<count($queryup);$d++){
-			$completet2 = "task_id";
-			$step2 = $d;
-			$set1="`$completet2` = '$step2'";
-			$example2 =$d+1;
-			$where2 ="`task_id`='$example2'";
-		#	$queryup = $db->update($name, $set1, $where2);
-			
-}}
-}
 	
-header('Location: ' . $_SERVER['HTTP_REFERER']);
+#header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
-header('Location: ' . $_SERVER['HTTP_REFERER'])
-}
+#header('Location: ' . $_SERVER['HTTP_REFERER'])
+
 echo $_POST["newtask"];
 
 ?>
