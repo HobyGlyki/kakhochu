fd182d9852f69126e4c4e0c2d1128fd07b3ba962
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-30 07:28:19 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -30,10 +30,10 @@ for($j=0;$j<count($query);$j++) {
 		$set1="`$text` = '$step'"; #соединить Text и Step
 #$where = "WHERE ".$where1;
 #$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
-		$queryup = $db->update($name, $set1, $where1);}}
+		$queryup = $db->update($name, $set1, $where1);}
 	else{
 		$queryup = $db->delete($name, $where1);
-}
+}}
 header('Location: ' . $_SERVER['HTTP_REFERER']);}
 
 
