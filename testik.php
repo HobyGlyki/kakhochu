6898d9e8649760db9dba7915dfeff2d2688784aa
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-30 12:55:33 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -3,20 +3,7 @@
 // Подключение файла соединения с БД
 
  // Подключение файла соединения с БД
-include_once 'db.class.php';
 
-
- // хост БД
-define('db_host','localhost:3306');
- // Имя БД
-define('db_name','dbate');
-// Пользователь БД
-define('db_user','admin');
-// Пароль БД
-define('db_pass','admin_password');
-
-$db = new DB_class(db_host,db_name,db_user,db_pass);
-$query = $db->select(true,"*","tasks","");
 $name="`tasks`"; # Название таблицы
 $maxx = count($query);
 
