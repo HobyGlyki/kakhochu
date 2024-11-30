01b9ccf663c43f578cee66dba4f6b41d9020aa6d
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-30 07:33:59 +0300

    Update new.php

---
+++ new.php
@@ -0,0 +1,40 @@
+<?php <?php
+
+// Подключение файла соединения с БД
+
+ // Подключение файла соединения с БД
+include_once 'db.class.php';
+
+
+ // хост БД
+define('db_host','localhost:3306');
+ // Имя БД
+define('db_name','dbate');
+// Пользователь БД
+define('db_user','admin');
+// Пароль БД
+define('db_pass','admin_password');
+?/>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Заголовок сайта</title>
+ <link rel="stylesheet" href="styles.css">
+</head>
+  
+<body>
+	<div class="header">
+ 	<div class="text_header">
+ 	<h1>Задачник</h1>
+ 	</div>
+</div>
+<div class="main-text">
+	<form action="testik.php" method="post">
+  </from>
+  </div>
+  </body>
+  </html>
+  
+  
