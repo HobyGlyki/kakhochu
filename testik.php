0986d89bdd3a20028d4456badd640e9b96a99a45
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-30 09:34:35 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -17,17 +17,20 @@ define('db_pass','admin_password');
 
 $db = new DB_class(db_host,db_name,db_user,db_pass);
 $query = $db->select(true,"*","tasks","");
-if ($_POST["newtask"]){
 $name="`tasks`"; # Название таблицы
-$text = "task_complete"; # Что поменять
-
+if ($_POST["newtask"]){
+$completet = "`task_id`, `task_text`, `task_date`, `task_complete`";
+$step = "`count($query)+2`,  `$_POST["textnew"]`, `$_POST["timenew"]`, `0`";
+$db->insert($name,$completet ,$step);}
+else{
 for($j=0;$j<count($query);$j++) {
+	$completet = "task_complete"; # Что поменять
 	$example =$j+1; #Значение условия
 	$where1= "`task_id`='$example'"; #условие
 	if ($_POST["check$j"] <> 4){
 		$step = $j;
 		$step = $_POST["check$j"]-1;# На что поменять
-		$set1="`$text` = '$step'"; #соединить Text и Step
+		$set1="`$completet` = '$step'"; #соединить Text и Step
 #$where = "WHERE ".$where1;
 #$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
 		$queryup = $db->update($name, $set1, $where1);}
