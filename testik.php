66673739625cd1193f6fa466913fca8ba9703929
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-30 11:27:09 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -40,7 +40,7 @@ for($j=0;$j<count($query);$j++) {
 			$set1="`$completet2` = '$step2'";
 			$example2 =$d+1;
 			$where2 ="`task_id`='$example2'";
-			$queryup = $db->update($name, $set1, $where2);
+			#$queryup = $db->update($name, $set1, $where2);
 		}}
 	header('Location: ' . $_SERVER['HTTP_REFERER']);
 		}
