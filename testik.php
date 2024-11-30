4b87415f0d9a7c1c9ab9f769076e65039a63d640
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-30 10:49:11 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -31,18 +31,7 @@ for($j=0;$j<count($query);$j++) {
 #$where = "WHERE ".$where1;
 #$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
 		$queryup = $db->update($name, $set1, $where1);}
-	else{
-		$queryup = $db->delete($name, $where1);
-		for($d=$j+1;$d < (count($queryup)-$j+1);$d++) {
-			$completet2 = "task_id";
-			$step2 = $d+1;
-			$set1="`$completet2` = '$step2'";
-			$example =$d+1;
-			$where2 ="`task_id`='$example'";
-			$queryup = $db->update($name, $set1, $where2);}}
-	header('Location: ' . $_SERVER['HTTP_REFERER']);
-		}
-}
+
 header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 
