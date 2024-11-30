567cdf40bc40e2be2493ee284b654ac1f15c87f5
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-30 12:50:38 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -25,33 +25,7 @@ if ($_POST["newtask"]=="true"){
 
 header('Location: ' . $_SERVER['HTTP_REFERER'])
 }
-elseif(1==0){	
-for($j=0;$j<count($query);$j++) {
-	$completet = "task_complete"; # Что поменять
-	$example =$j+1; #Значение условия
-	$where1= "`task_id`='$example'"; #условие
-		$step = $_POST["check$j"]-1;# На что поменять
-		$set1="`$completet` = '$step'"; #соединить Text и Step
-#$where = "WHERE ".$where1;
-#$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
-		$queryup = $db->update($name, $set1, $where1);
-
-	if(($_POST["del"]==$j+1) and ($maxx > 1)){
-		$queryup = $db->delete($name, $where1);
-		$queryup = $db->select(true,"*","tasks","");
-		for($d=$j;$d<count($queryup);$d++){
-			$completet2 = "task_id";
-			$step2 = $d;
-			$set1="`$completet2` = '$step2'";
-			$example2 =$d+1;
-			$where2 ="`task_id`='$example2'";
-			$queryup = $db->update($name, $set1, $where2);
-			
-}}
 }
-	
-header('Location: ' . $_SERVER['HTTP_REFERER']);
-}}
 #echo $_POST["del"];
 
 ?>
