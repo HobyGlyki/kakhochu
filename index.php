403d00354d3a01e1f90c009f50e295d94b4ea85b
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-11-30 11:50:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ else{
                 <input type="date" name="time" value="<?php echo $query[$i]['task_date'] ?>">
 
 	<div class="task-control", a>
-	<select name=<?php echo "check".$i?> class="select" size="4">
+	<select name=<?php echo "check".$i?> class="select" size="3">
 	<option class="none" <?php if($ched[$i]==0)
 {echo "selected";} ?> value="1">Готовится</option>
 	<option class="aply"  <?php if($ched[$i]==1)
