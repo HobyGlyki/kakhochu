d5e5d5cdd5462e77c3a0a65d9aa24f35b3959759
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-12-05 13:39:44 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -65,7 +65,7 @@ for($j=0;$j<count($query);$j++) {
 				}}}
 if (true){
 for($d=0;$d<count($delid);$d++){
-	echo ($delid[0]=="");
+	echo (is_null($delid[0]));
 	#$queryupd = $db->update($name, $delid[$d]);
 }}	
 }
