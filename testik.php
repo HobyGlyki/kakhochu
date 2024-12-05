9d4ff8ecaf9c5462cb6196f55a7e5a895803f7e2
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-12-05 12:37:42 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -34,16 +34,8 @@ $update = $db->insert($name,$completet ,$step);
 #echo "'$idtask', '$texttask', '$datetask', '0'";
 #}}
 
-	elseif(not ($_POST["newtask"])){	
-for($j=0;$j<count($query);$j++) {
-	$completet = "task_complete"; # Что поменять
-	$example =$j+1; #Значение условия
-	$where1= "`task_id`='$example'"; #условие
-		$step = $_POST["check$j"]-1;# На что поменять
-		$set1="`$completet` = '$step'"; #соединить Text и Step
-#$where = "WHERE ".$where1;
-#$update_sql1 = "UPDATE ".$name." SET ".$set1." ".$where."";
-		$queryup = $db->update($name, $set1, $where1);}}
+	elseif(not ($_POST["newtask"]== true)){	
+}
 
 	#if(($_POST["del"]==$j+1) and ($maxx > 1)){
 	#	$queryup = $db->delete($name, $where1);
