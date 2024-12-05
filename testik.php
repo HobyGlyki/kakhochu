fa788daeceecfb7f71d07448a4dd27431a952ea3
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-12-05 13:26:10 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -13,6 +13,7 @@ define('db_pass','admin_password');
 
 $db = new DB_class(db_host,db_name,db_user,db_pass);
 $query = $db->select(true,"*","tasks","");
+$delid= array();
 // Подключение файла соединения с БД
 
  // Подключение файла соединения с БД
@@ -59,10 +60,13 @@ for($j=0;$j<count($query);$j++) {
 			$set1="`$completet2` = '$step2'";
 			$example2 =$d+1;
 			$where2= "`task_id`='$example2'";
-			echo $set1. "не сука";
-			echo count($queryup);
+			$delid[]= "'$set1', '$where2'";
+
 #			$queryupd = $db->update($name, $set1, $where2);
-				}}}}
+				}
+for($d=0;$d<count($delid);$d++){
+	echo $delid[];
+	}}}
 			
 #}}
 #}
