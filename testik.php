20406711bd5c75b5d2d83d43de1f7148d6d330b1
HobyGlyki <73791514+HobyGlyki@users.noreply.github.com>
2024-12-05 12:40:35 +0300

    Update testik.php

--- testik.php
+++ testik.php
@@ -34,7 +34,7 @@ $update = $db->insert($name,$completet ,$step);
 #echo "'$idtask', '$texttask', '$datetask', '0'";
 #}}
 
-elseif(($_POST["newtask"]== true)){	
+elseif(not($_POST["newtask"]== true)){	
 	for($j=0;$j<count($query);$j++) {
 		$completet = "task_complete"; # Что поменять
 		$example =$j+1; #Значение условия
